c713c6699fcb794d6df4643d6c76c1d1c7d26733
Elias Ortega <eliasortega2011@gmail.com>
2023-06-27 22:25:54 -0400

    lectura datos sin fs, consulta api, destructuracion del objeto

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
-const datosPokeAPI = require('./pokemones/datosPokeAPI');
-const datosAPIRandom = require('./randomAPI/apiRandom');
+const axios = require('axios');
 
-console.log(datosPokeAPI);
-console.log(datosAPIRandom); 
+const datosArrayPoke = require('./pokemones/datosPokeAPI');
+
+const datosArrayAPIRandom = require('./randomAPI/apiRandom');
+
+const obtenerPokemon = async (nombre) => {
+  try {
+    const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${nombre}`);
+    return response.data;
+  } catch (error) {
+    console.log(`Error al obtener los datos del Pokémon ${nombre}`, error);
+    return null;
+  }
+}
+
+const construirMensajePokemon = (pokemon) => {
+  const { name, base_experience,  order, weight } = pokemon;
+  return `Pokémon: ${name}\nExperiencia: ${base_experience}\nOrden: ${order}\nPeso: ${weight}`;
+}
+
+// Ejemplo Pokemon 
+const nombrePokemon = 'pikachu';
+obtenerPokemon(nombrePokemon).then((pokemon) => {
+  if (pokemon) {
+    const mensajePokemon = construirMensajePokemon(pokemon);
+    console.log(mensajePokemon);
+  }
+});
+
+console.log(datosArrayPoke);
+console.log(datosArrayAPIRandom);
--- pokemones/datosPokeAPI.js
+++ pokemones/datosPokeAPI.js
@@ -1,6 +1,6 @@
-const fs = require('fs');
-
-const datosPokemones = fs.readFileSync('./pokemones/datosPoke.txt', 'utf-8');
-const datosPokeAPIArray = JSON.parse(datosPokemones);
-
-module.exports = datosPokeAPIArray;
+const datosArrayPoke = [
+    "bulbasaur", "ivysaur", "venusaur", "squirtle", "wartortle", "blastoise", "charmander", "charmeleon", "charizard"
+  ];
+  
+  module.exports = datosArrayPoke;
+  
